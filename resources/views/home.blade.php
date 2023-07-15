ba60af5f2c08fd51ced8cccbba6ed5af3c417b08
IlmanNK <ilmanponka345@gmail.com>
2023-07-16 02:46:09 +0700

    Menambahkan content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,82 +5,97 @@
     @include('partial.bg')
 @endsection
 @section('content2')
-<br>
+    <br>
 
-<div class="d-flex justify-content-center align-items-center" style="font-family: 'Franklin Gothic Medium', sans-serif">
-    <h1 class="text-center fw-bold bg-black text-light p-3" >Dapatkan Rilisan Terbaru</h1>
-  </div>
-<br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="jumbroton">
+                <p>Selamat datang di Aphelion Store, destinasi yang sempurna untuk menemukan sepatu vulcanize, sneakers, dan
+                    slip-on yang akan memenuhi kebutuhan gaya dan kenyamanan Anda. Dengan desain klasik dan sentuhan retro,
+                    sepatu vulcanize kami menghadirkan pesona yang tak tergoyahkan, sementara sneakers kami menggabungkan
+                    teknologi modern dengan gaya yang dinamis. Untuk kenyamanan sehari-hari tanpa ribet mengikat tali
+                    sepatu, slip-on kami adalah pilihan yang tepat. Jangan lewatkan penawaran spesial kami, termasuk diskon
+                    20% untuk pembelian pertama Anda dengan kode promosi "APH20". Nikmati juga pengiriman gratis untuk semua
+                    pembelian. Kunjungi Aphelion Store sekarang dan temukan sepatu yang memancarkan semangat petualangan dan
+                    menghadirkan gaya yang tak tertandingi.</p>
+            </div>
+        </div>
+    </div>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-4">
-            <div class="col mb-5">
-                <div class="card h-100">
-                    <!-- Product image-->
-                    <img class="card-img-top" src="../assets/foto/Vulcanize.png" alt="Vulcanize" />
-                    <!-- Product details-->
-                    <div class="card-body p-4">
-                        <div class="text-center">
-                            <!-- Product name-->
-                            <h1 class="fw-bolder" style="font-family: Bebas Neue">Vulcanize</h1>
-                            <!-- Product price-->
+    <div class="d-flex justify-content-center align-items-center" style="font-family: 'Franklin Gothic Medium', sans-serif">
+        <h1 class="text-center fw-bold bg-black text-light p-3">Dapatkan Rilisan Terbaru</h1>
+    </div>
+    <br>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="../assets/foto/Vulcanize.png" alt="Vulcanize" />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h1 class="fw-bolder" style="font-family: Bebas Neue">Vulcanize</h1>
+                                <!-- Product price-->
+                            </div>
                         </div>
-                    </div>
-                    <!-- Product actions-->
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Liat Produk</a>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Liat Produk</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-		</div>
-		<div class="col-md-4">
-            <div class="col mb-5">
-                <div class="card h-100">
-                    <!-- Product image-->
-                    <img class="card-img-top" src="../assets/foto/Sneakers.png" alt="Sneakers" />
-                    <!-- Product details-->
-                    <div class="card-body p-4">
-                        <div class="text-center">
-                            <!-- Product name-->
-                            <h1 class="fw-bolder" style="font-family: Bebas Neue">Sneakers</h1>
-                            <!-- Product price-->
+            <div class="col-md-4">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="../assets/foto/Sneakers.png" alt="Sneakers" />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h1 class="fw-bolder" style="font-family: Bebas Neue">Sneakers</h1>
+                                <!-- Product price-->
 
+                            </div>
                         </div>
-                    </div>
-                    <!-- Product actions-->
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Lihat Produk</a>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Lihat Produk</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-		</div>
-		<div class="col-md-4">
-            <div class="col mb-5">
-                <div class="card h-100">
-                    <!-- Product image-->
-                    <img class="card-img-top" src="../assets/foto/Slip On.png" alt="Slip On" />
-                    <!-- Product details-->
-                    <div class="card-body p-4">
-                        <div class="text-center">
-                            <!-- Product name-->
-                            <h1 class="fw-bolder" style="font-family: Bebas Neue">Slip On</h1>
-                            <!-- Product price-->
+            <div class="col-md-4">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="../assets/foto/Slip On.png" alt="Slip On" />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h1 class="fw-bolder" style="font-family: Bebas Neue">Slip On</h1>
+                                <!-- Product price-->
 
+                            </div>
                         </div>
-                    </div>
-                    <!-- Product actions-->
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Lihat Produk</a>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Lihat Produk</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-		</div>
-	</div>
-</div>
+        </div>
+    </div>
     {{-- <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
--- resources/views/produk/NewProduk.blade.php
+++ resources/views/produk/NewProduk.blade.php
@@ -1,42 +1,49 @@
 @extends('template.toko.index')
 
 @section('content')
-<br>
+    <br>
 
-<div class="container-fluid">
-		<div class="col-md-10">
-            <div class="container-fluid">
-                <div class="row">
-                    @foreach ($produk as $product)
-                        <div class="col-md-4 mb-5">
-                            <div class="card h-100">
-                                <!-- Product image -->
-                                <img class="card-img-top" src="../assets/foto/{{ $product->foto }}" alt="{{ $product->nama_produk }}" />
-                                <!-- Product details -->
-                                <div class="card-body p-4">
-                                    <div class="text-center">
-                                        <!-- Product name -->
-                                        <h3 class="fw-bolder" style="font-family: Bebas Neue">{{ $product->nama_produk }}</h3>
-                                        <!-- Product price -->
-                                        <p>Harga: RP. {{ $product->harga_produk }}</p>
-                                        <!-- Product stock -->
-                                        <p>Stok: {{ $product->stok_produk }}</p>
-                                    </div>
+    </div>
+    <div class="bg-image bg-opacity-75 d-flex align-items-center justify-content-center vw-90 mt-3">
+        <img src="../assets/foto/Banner.png" alt="Banner" class="img-fluid">
+    </div>
+    
+    
+    <br>
+    <br>
+    <div class="container-fluid ">
+        <div class="col-md-10 mx-auto">
+            {{-- <div class="container-fluid"> --}}
+            <div class="row">
+                @foreach ($produk as $product)
+                    <div class="col-md-4 mb-5">
+                        <div class="card h-100">
+                            <!-- Product image -->
+                            <img class="card-img-top" src="../assets/foto/{{ $product->foto }}"
+                                alt="{{ $product->nama_produk }}" />
+                            <!-- Product details -->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name -->
+                                    <h3 class="fw-bolder" style="font-family: Bebas Neue">{{ $product->nama_produk }}
+                                    </h3>
+                                    <!-- Product price -->
+                                    <p>Harga: RP. {{ $product->harga_produk }}</p>
+                                    <!-- Product stock -->
+                                    <p>Stok: {{ $product->stok_produk }}</p>
                                 </div>
-                                <!-- Product actions -->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center">
-                                        <a class="btn btn-outline-dark mt-auto" href="#">Beli Sekarang</a>
-                                    </div>
+                            </div>
+                            <!-- Product actions -->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center">
+                                    <a class="btn btn-outline-dark mt-auto" href="#">Beli Sekarang</a>
                                 </div>
                             </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             </div>
-		</div>
-	</div>
-</div>
-
-
+            {{-- </div> --}}
+        </div>
+    </div>
 @endsection
