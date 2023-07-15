9c726a84416abf61b19732456c41c9a51aa15866
Dimasriooo <dd8308451@gmail.com>
2023-07-16 02:40:56 +0700

    penambahan role

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -24,7 +24,19 @@
                     <span class="badge bg-black text-white ms-1 rounded-pill">0</span>
                 </button>
             </form>
+        
             
+            @if (Auth::user()->role == 'admin')
+      <a href=" {{ route('produk.admin') }}" class="btn btn-primary mx-3 my-3">ADMIN</a>
+      @else
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+      </form>
+      @endif
+
+
+    
         </div>
     </div>
 </nav>
